3fa76895ce1ed99e7cd18617b7a1e1ed164d947f
Kyle Strout <62024550+KyleStrout@users.noreply.github.com>
2021-06-22 21:00:11 -0400

    welcome update

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -16,11 +16,12 @@ module.exports = async (client, Discord, member) => {
     //let welcomeRole = member.guild.roles.cache.find(role => role.name === 'Soldier')
     //await member.roles.add(welcomeRole)
 
-    welcomeEmbed = new MessageEmbed()
-        .setTitle(':wave: Welcome to Games & Sleep!')
-        .setDescription(`**${member.user.username}** just joined!\nUse \`/\` to use the bot`)
-        .setColor(EmbedColors.Discord.YELLOW)
-    await member.guild.channels.cache.get('839092761584074832').send({ embeds: [welcomeEmbed] })
+    // embed doesn't work for some reason here?
+    // welcomeEmbed = new MessageEmbed()
+    //     .setTitle(':wave: Welcome to Games & Sleep!')
+    //     .setDescription(`**${member.user.username}** just joined!\nUse \`/\` to use the bot`)
+    //     .setColor(EmbedColors.Discord.YELLOW)
+    // await member.guild.channels.cache.get('839092761584074832').send({ embeds: [welcomeEmbed] })
     await member.guild.channels.cache.get('839092761584074832').send(`Welcome **${member.user.username}!** \nType \`/help\` to use the bot!`)
 
     //383003302427754509
